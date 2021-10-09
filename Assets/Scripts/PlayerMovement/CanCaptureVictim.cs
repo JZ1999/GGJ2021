1624c2393ed6b2aecbee75d76b849f6e822f6f91
mandrix <ale.aazv@icloud.com>
2021-10-09 03:34:38 -0500

    movement player

---
+++ Assets/Scripts/PlayerMovement/CanCaptureVictim.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanCaptureVictim : MonoBehaviour
+{
+    public GameObject UiButton;
+    public GameObject victim; 
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Victims"))
+        {
+            victim = other.gameObject;
+            UiButton.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Victims"))
+        {
+            victim = null;
+            UiButton.SetActive(false);
+        }
+    }
+    public void Capture()
+    {
+        Destroy(victim);
+    }
+}
