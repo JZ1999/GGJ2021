eaf3b96dafb71c6ed79b150e847b62f9ec5aac6c
mandrix <ale.aazv@icloud.com>
2021-10-09 02:08:36 -0500

    movement

---
+++ Assets/Scripts/PlayerMovement/PlayerRotate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotate : MonoBehaviour
+{
+    public VariableJoystick variableJoystick;
+    public Vector3 y = Vector3.zero;
+
+    public void FixedUpdate()
+    {
+        transform.Rotate(y + new Vector3(0, variableJoystick.Horizontal, 0), Space.Self);
+        Vector3 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
+    }
+}
