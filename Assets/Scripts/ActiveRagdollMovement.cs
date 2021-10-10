ba2dbb901cb7a0e8b775c349beaae23cf4233619
Briam David Mora Villalobos <briamtec@gmail.com>
2021-10-10 08:29:32 -0600

    ragdoll

---
+++ Assets/Scripts/ActiveRagdollMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveRagdollMovement : MonoBehaviour
+{
+
+    public Transform[] bodyParts;
+    public ConfigurableJoint[] joints;
+    public Quaternion[] initialRotations;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialRotations = new Quaternion[bodyParts.Length];
+        for (int i = 0 ; i < bodyParts.Length ; i++ ){
+            initialRotations[i] = bodyParts[i].localRotation;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0 ; i < joints.Length ; i++ ){
+           // ConfigurableJointExtensions.SetTargetRotationLocal(joints[i],bodyParts[i].localRotation,initialRotations[i]);
+        }
+    }
+}
