957081085fdf650fac75457d1a543d180329a4a3
mandrix <ale.aazv@icloud.com>
2021-10-09 19:09:43 -0500

    added sound bar

---
+++ Assets/Scripts/Managers/WinLoseManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinLoseManager : MonoBehaviour
+{
+    [Range(100, 200)]
+    public float maxSoundForAwaking;
+    public float soundForAwaking;
+    public Image uiBarFront;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiBarFront.fillAmount = 0f;
+    }
+
+    public void AddSum(float added)
+    {
+
+        soundForAwaking += added;
+        uiBarFront.fillAmount = soundForAwaking / maxSoundForAwaking;
+
+        if (soundForAwaking >= maxSoundForAwaking)
+        {
+            Awaking();
+        }
+    }
+
+    public void Awaking()
+    {
+        Debug.Log("se despertó el humano, victimas han ganado");
+    }
+}
